1db7ebd4374b39938cc5cb477901bf0fa586865d
Jon Shaw <jonshaw199@gmail.com>
2024-09-24 14:00:12 -0700

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Default from "./Default";
+import ThreeJsExample from "./ThreeJsExample";
+
+function App() {
+  return (
+    <>
+      <Default />
+      <ThreeJsExample />
+    </>
+  );
+}
+
+export default App;
