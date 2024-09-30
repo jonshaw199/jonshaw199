b2121ce27bacacd2d1bf50ee836cbec7b2a964b8
Jon Shaw <jonshaw199@gmail.com>
2024-09-30 16:50:55 -0700

    ; add content to AboutMe component

--- src/components/about-me/AboutMe.tsx
+++ src/components/about-me/AboutMe.tsx
@@ -1,3 +1,25 @@
+import { Button } from 'react-bootstrap';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
 export default function AboutMe() {
-  return <>About Me</>;
+  return (
+    <div
+      className="p-3 h-100 d-flex flex-column justify-content-center"
+      style={{ color: "whitesmoke", fontSize: 18 }}
+    >
+      <p>Hi there!</p>
+      <p>
+        I’m Jon, a dedicated software engineer with a track record of
+        delivering impactful solutions across multiple platforms—whether it’s
+        web, mobile, or embedded systems. With a focus on quality, performance,
+        and user experience, I’ve been fortunate to work on diverse technologies
+        that push the boundaries of innovation. Please explore my work, and
+        let’s connect if you’d like to collaborate!.
+      </p>
+        <div className="d-flex gap-4 justify-content-center">
+          <Button href="https://linkedin.com/in/jonshaw199" variant="ghost" size="lg" className="p-0"><FaLinkedin color="white" /></Button>
+          <Button href="https://github.com/jonshaw199" variant="ghost" size="lg" className="p-0"><FaGithub color="white" /></Button>
+        </div>
+    </div>
+  );
 }
