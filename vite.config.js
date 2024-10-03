c5fcda22f8815d3b68fef244ffc48b83ef9d5bd4
Jon Shaw <jonshaw199@gmail.com>
2024-10-03 01:18:24 -0700

    allow LAN access in local dev

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": "/src",
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
     },
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
 });
